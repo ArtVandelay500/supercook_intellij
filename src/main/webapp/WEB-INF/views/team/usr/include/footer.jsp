02e3d712aeedadf9370065e3be0588bf279bebbc
ArtVandelay500 <ArtVandelay500@users.noreply.github.com>
2023-07-15 10:22:20 +0900

    basic structure of intellij Spring

--- src/main/webapp/WEB-INF/views/team/usr/include/footer.jsp
+++ src/main/webapp/WEB-INF/views/team/usr/include/footer.jsp
@@ -9,14 +9,22 @@
         <div class="teammatesInfo">
             <h4>Developers</h4>
 
-            <a id="song" href="#"><span>송재윤</span></a>
-            <a id="lee" href="#"><span>이승민</span></a>
-            <a id="hong" href="#"><span>홍현욱</span></a>
+            <a class="song" href="#"><span>송재윤</span></a>
+            <a class="lee" href="#"><span>이승민</span></a>
+            <a class="hong" href="#"><span>홍현욱</span></a>
 
         </div>
     </div>
     <div class="personalPrj">
-        <div id="hongg" class="personalPrjEach">
+        <div class="personalPrjEach index">
+            <div class="footerLogo personalLogo">
+                <img src="/resources/team/img/yummy_color.png" alt="'JOIN' Team Logo image located"/>
+            </div>
+            <p>
+                인덱스 입니다
+            </p>
+        </div>
+        <div class="personalPrjEach hongg">
             <div class="footerLogo personalLogo">
                 <img src="/resources/team/img/yummy_color.png" alt="'JOIN' Team Logo image located"/>
             </div>
@@ -24,12 +32,20 @@
                 만개의 레시피와 같은<br> 레시피 제공 사이트입니다
             </p>
         </div>
-        <div id="songg" class="personalPrjEach">
+        <div class="personalPrjEach songg">
             <div class="footerLogo personalLogo">
                 <img src="/resources/team/img/yummy_color.png" alt="'JOIN' Team Logo image located"/>
             </div>
             <p>
-                이건 뭐 나는 어디?
+                나는 송재윤입니다
+            </p>
+        </div>
+        <div class="personalPrjEach leee">
+            <div class="footerLogo personalLogo">
+                <img src="/resources/team/img/yummy_color.png" alt="'JOIN' Team Logo image located"/>
+            </div>
+            <p>
+                나는 이승민입니다
             </p>
         </div>
     </div>
--- src/main/webapp/resources/team/js/footer.js
+++ src/main/webapp/resources/team/js/footer.js
@@ -1,27 +1,83 @@
 $(function(){
-    $("#hongg").hide();
+    $(".hongg").hide();
+    $(".songg").hide();
+    $(".leee").hide();
     // $(".leee").hide();
 
-
-    $("#hong").on("mouseenter",function(){
+    // 홍현욱
+    $(".hong").on("click",function(){
         // all indented li reset
-        $(this).parent("div").siblings("a").css({
+        $(this).siblings("a").css({
             letterSpacing : "-1px",
+            color: "black"
         });
         // if clicked, clicked li indented
         $(this).css({
             letterSpacing : "0px",
-            color:"red",
+            color:"rgba(26, 103, 26, 0.95)",
         });
         // if any of each sections inside of the "main container" is currently opened, close it
         $(".personalPrj").children("div").each(function(index,item){
             if($(item).is(":visible")){
                 $(this).fadeOut(function(){
                     // and open the clicked section
-                    $("#hongg").fadeIn();
+                    $(".hongg").stop().fadeIn();
                 });
             }
         });
 
     });
-});
+    // 홍현욱
+
+    // 송재윤
+    $(".song").on("click",function(){
+        // all indented li reset
+        $(this).siblings("a").css({
+            letterSpacing : "-1px",
+            color: "black"
+        });
+        // if clicked, clicked li indented
+        $(this).css({
+            letterSpacing : "0px",
+            color:"rgba(26, 103, 26, 0.95)",
+        });
+        // if any of each sections inside of the "main container" is currently opened, close it
+        $(".personalPrj").children("div").each(function(index,item){
+            if($(item).is(":visible")){
+                $(this).fadeOut(function(){
+                    // and open the clicked section
+                    $(".songg").stop().fadeIn();
+                });
+            }
+        });
+
+    });
+    // 송재윤
+
+    // 이승민
+    $(".lee").on("click",function(){
+        // all indented li reset
+        $(this).siblings("a").css({
+            letterSpacing : "-1px",
+            color: "black"
+        });
+        // if clicked, clicked li indented
+        $(this).css({
+            letterSpacing : "0px",
+            color:"rgba(26, 103, 26, 0.95)",
+        });
+        // if any of each sections inside of the "main container" is currently opened, close it
+        $(".personalPrj").children("div").each(function(index,item){
+            if($(item).is(":visible")){
+                $(this).fadeOut(function(){
+                    // and open the clicked section
+                    $(".leee").stop().fadeIn();
+                });
+            }
+        });
+
+    });
+    // 이승민
+
+
+}); //jQuery function closing tag
