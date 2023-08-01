48cbcb7f111b4172c35288a4aed03b46f5cf84c5
ArtVandelay500 <ArtVandelay500@users.noreply.github.com>
2023-08-01 14:07:36 +0900

    file upload profile

--- src/main/java/com/vandelay/app/controller/CovidController.java
+++ src/main/java/com/vandelay/app/controller/CovidController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CovidController {
     @RequestMapping("/covid")
     public String indexCovid(){
+
+        String apiURL = "https://apis.data.go.kr/1471000/CovidDagnsRgntProdExprtStusService/getCovidDagnsRgntProdExprtStusInq?serviceKey=lLcMxW2C0vP%2FMqJspFD4sy5ffJ2QQbWT671ZE%2FJAQxMx2h4fSHrmERaDVHVGQ3Y8MgeDgMiGyLRj5JCa5P%2FTVg%3D%3D&type=json";
+
         return "covidTest";
     }
 }
